649d2164b65719dd3761a42c7581cbdf62a6fdad
jhill144 <jhill144@student.gsu.edu>
2024-06-22 08:33:02 -0400

    Adding signin module from modular ui

--- lib/views/authentication/login.dart
+++ lib/views/authentication/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:modular_ui/modular_ui.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key, required this.title});
@@ -12,16 +13,13 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
-      ),
+          child: MUISignInCard(
+        emailController: TextEditingController(),
+        passwordController: TextEditingController(),
+        onSignInPressed: () async {},
+        onRegisterNow: () {},
+      )),
     );
   }
 }

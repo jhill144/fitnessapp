6c3dd24a212ed5b001484b885b57fda4028c8a41
joeyonathan <jyonathan1@student.gsu.edu>
2024-06-30 13:35:49 -0400

    - Added function `getWorkouts` in `fitnessworkout.dart` to return a list of `Fitnessworkout` objects. - Updated `generateWorkOutsList` in `fitnessworkout.dart` to use the `getWorkouts` function. - Modified `workout.dart` to use the `getWorkouts` function for filtering workouts by category. - Implemented `TabBar` and `TabBarView` in `workout.dart` for categorized workout display. - Ensured navigation to `workoutdetail.dart` works correctly.

---
+++ lib/views/progresstracker.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:fitnessapp/views/navigation_menu.dart';
+
+class ProgressTrackerPage extends StatefulWidget {
+  const ProgressTrackerPage({super.key, required this.title});
+
+  final String title;
+  @override
+  State<ProgressTrackerPage> createState() => _ProgressTrackerPageState();
+}
+
+class _ProgressTrackerPageState extends State<ProgressTrackerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      bottomNavigationBar: const NavigationBanner(
+        title: 'navigation',
+      ),
+      body: Center(),
+    );
+  }
+}
--- lib/views/workouts/workout.dart
+++ lib/views/workouts/workout.dart
@@ -1,33 +1,83 @@
-import 'package:fitnessapp/main.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:modular_ui/modular_ui.dart';
-import 'package:fitnessapp/views/navigation_menu.dart';
 import 'package:fitnessapp/models/fitnessworkout.dart';
+import 'package:fitnessapp/utilities/enums.dart';
+import 'package:go_router/go_router.dart';
+import 'package:fitnessapp/utilities/route_constants.dart';
 
 class WorkoutPage extends StatefulWidget {
-  const WorkoutPage({super.key, required this.title});
+  const WorkoutPage({Key? key, required this.title}) : super(key: key);
 
   final String title;
+
   @override
-  State<WorkoutPage> createState() => _WorkoutPageState();
+  _WorkoutPageState createState() => _WorkoutPageState();
 }
 
-class _WorkoutPageState extends State<WorkoutPage> {
+class _WorkoutPageState extends State<WorkoutPage> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  final List<Fitnessworkout> _workouts = getWorkouts();
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 6, vsync: this);
+  }
+
+  List<Fitnessworkout> _getWorkoutsByCategory(WorkoutCategory category) {
+    return _workouts.where((workout) => workout.workoutCategory == category).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(text: "Chest"),
+            Tab(text: "Shoulders"),
+            Tab(text: "Back"),
+            Tab(text: "Arms"),
+            Tab(text: "Legs"),
+            Tab(text: "Cardio"),
+          ],
+        ),
       ),
-      body: Center(
-        child: generateWorkOutsList(),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          _buildWorkoutList(WorkoutCategory.chest),
+          _buildWorkoutList(WorkoutCategory.shoulders),
+          _buildWorkoutList(WorkoutCategory.back),
+          _buildWorkoutList(WorkoutCategory.arms),
+          _buildWorkoutList(WorkoutCategory.legs),
+          _buildWorkoutList(WorkoutCategory.cardio),
+        ],
       ),
-      backgroundColor: Color.fromARGB(255, 255, 255, 255),
-      bottomNavigationBar: const NavigationBanner(
-        title: 'navigation',
-      ),
+    );
+  }
+
+  Widget _buildWorkoutList(WorkoutCategory category) {
+    final workouts = _getWorkoutsByCategory(category);
+    return ListView.builder(
+      itemCount: workouts.length,
+      itemBuilder: (context, index) {
+        return Card(
+          elevation: 3,
+          child: ListTile(
+            title: Text(
+              workouts[index].workoutName,
+              style: TextStyle(color: Colors.black),
+            ),
+            subtitle: Text(workouts[index].workoutDescription),
+            leading: workouts[index].workoutThumbnail,
+            onTap: () {
+              context.pushNamed(RouteConstants.workoutdetails, extra: workouts[index]);
+            },
+          ),
+        );
+      },
     );
   }
 }

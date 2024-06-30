6771ce81156f85ef305e8c05af91ed465af0190b
pocscz06 <158521706+pocscz06@users.noreply.github.com>
2024-06-30 19:40:17 -0400

    Created theme_provider.dart for toggling themes in settings

---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider with ChangeNotifier {
+  bool _isDarkTheme = false;
+
+  bool get isDarkTheme => _isDarkTheme;
+
+  ThemeProvider() {
+    _loadTheme();
+  }
+
+  void _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDarkTheme = prefs.getBool('isDarkTheme') ?? false;
+    notifyListeners();
+  }
+
+  void toggleTheme() async {
+    _isDarkTheme = !_isDarkTheme;
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDarkTheme', _isDarkTheme);
+    notifyListeners();
+  }
+}

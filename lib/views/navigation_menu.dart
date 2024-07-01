e621ace1669aa49a2c4b7e8ce20b49d589a127af
pocscz06 <158521706+pocscz06@users.noreply.github.com>
2024-07-01 10:12:07 -0400

    Modified navigation_menu.dart so that NavigationMenu provided by Fluter is used rather than google_nav_bar; integrated theme switching with the navigation menu; removed the "home" page and made the "workout" page the home page.

--- lib/views/navigation_menu.dart
+++ lib/views/navigation_menu.dart
@@ -1,37 +1,69 @@
-import 'package:fitnessapp/utilities/route_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:provider/provider.dart';
+import 'package:fitnessapp/providers/theme_provider.dart';
+import 'package:fitnessapp/utilities/route_constants.dart';
 
-class NavigationBanner extends StatefulWidget {
-  const NavigationBanner({super.key, required this.title});
-
+class NavigationMenu extends StatelessWidget {
   final String title;
-  @override
-  State<NavigationBanner> createState() => _NavigationBannerState();
-}
 
-class _NavigationBannerState extends State<NavigationBanner> {
+  const NavigationMenu({super.key, required this.title});
+
   @override
   Widget build(BuildContext context) {
-    return GNav(
-        onTabChange: (index) {
-          switch (index) {
-            case 1:
-              context.push(RouteConstants.workout);
-            case 2:
-              context.push(RouteConstants.profile);
-            case 3:
-              context.push(RouteConstants.user);
-            default:
-              context.push(RouteConstants.home);
-          }
-        },
-        tabs: [
-          GButton(icon: Icons.home),
-          GButton(icon: Icons.hdr_strong),
-          GButton(icon: Icons.person),
-          GButton(icon: Icons.settings),
-        ]);
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return BottomNavigationBar(
+      backgroundColor: themeProvider.isDarkTheme ? Colors.black : Colors.white,
+      selectedItemColor:
+          themeProvider.isDarkTheme ? Colors.white : Colors.black,
+      unselectedItemColor:
+          themeProvider.isDarkTheme ? Colors.white70 : Colors.black54,
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.fitness_center),
+          label: 'Workouts',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.track_changes),
+          label: 'Progress Tracker',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          label: 'Settings',
+        ),
+      ],
+      currentIndex: _getCurrentIndex(context),
+      onTap: (index) {
+        _onItemTapped(context, index);
+      },
+    );
+  }
+
+  int _getCurrentIndex(BuildContext context) {
+    final location =
+        GoRouter.of(context).routerDelegate.currentConfiguration.fullPath;
+    if (location.startsWith(RouteConstants.workout)) {
+      return 0;
+    } else if (location.startsWith(RouteConstants.progresstracker)) {
+      return 1;
+    } else if (location.startsWith(RouteConstants.settings)) {
+      return 2;
+    }
+    return 0;
+  }
+
+  void _onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.goNamed(RouteConstants.workout);
+        break;
+      case 1:
+        context.push(RouteConstants.progresstracker);
+        break;
+      case 2:
+        context.push(RouteConstants.settings);
+        break;
+    }
   }
 }

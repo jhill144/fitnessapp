89bd262b493e3730cdab9ca63a70eadcab83a0b6
jhill144 <jhill144@student.gsu.edu>
2024-07-01 22:35:35 -0400

    Database Working

--- lib/views/settings_screen.dart
+++ lib/views/settings_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:provider/provider.dart';
 import 'package:fitnessapp/providers/theme_provider.dart';
+import 'package:fitnessapp/utilities/database_helper.dart';
+import 'package:sqflite/sqflite.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -25,12 +27,23 @@ class _SettingsScreenState extends State<SettingsScreen> {
 
   Future<void> loadSettings() async {
     final prefs = await SharedPreferences.getInstance();
-    setState(() {
-      goalController.text = prefs.getString('goal') ?? '';
-      ageController.text = prefs.getString('age') ?? '';
-      weightController.text = prefs.getString('weight') ?? '';
-      heightController.text = prefs.getString('height') ?? '';
-      isDarkTheme = prefs.getBool('isDarkTheme') ?? false;
+    Database dbHelper = await DatabaseHelper.instance.database;
+
+    final mapQuery = await dbHelper.rawQuery(
+        'SELECT * FROM ${DatabaseHelper.table} WHERE ${DatabaseHelper.columnId} = 1');
+
+    mapQuery.forEach((dbitem) {
+      setState(() {
+        goalController.text = prefs.getString('goal') ??
+            dbitem[DatabaseHelper.columnGoal].toString();
+        ageController.text = prefs.getString('age') ??
+            dbitem[DatabaseHelper.columnAge].toString();
+        weightController.text = prefs.getString('weight') ??
+            dbitem[DatabaseHelper.columnWeight].toString();
+        heightController.text = prefs.getString('height') ??
+            dbitem[DatabaseHelper.columnHeightFeet].toString();
+        isDarkTheme = prefs.getBool('isDarkTheme') ?? false;
+      });
     });
   }
 
@@ -48,9 +61,31 @@ class _SettingsScreenState extends State<SettingsScreen> {
       themeProvider.toggleTheme();
     }
 
+    _update();
+
     // Show a snackbar to confirm settings are saved
     ScaffoldMessenger.of(context)
         .showSnackBar(const SnackBar(content: Text('Settings saved')));
+  }
+
+  void _update() async {
+// row to insert
+    Map<String, dynamic> row = {
+      DatabaseHelper.columnName: 'Bob',
+      DatabaseHelper.columnAge: int.parse(ageController.text),
+      DatabaseHelper.columnWeight: double.parse(weightController.text),
+      DatabaseHelper.columnHeightFeet: double.parse(heightController.text),
+      DatabaseHelper.columnHeightInch: double.parse('0'),
+      DatabaseHelper.columnGoal: goalController.text,
+    };
+
+    Database dbHelper = await DatabaseHelper.instance.database;
+
+    final id = await dbHelper.update(DatabaseHelper.table, row,
+        where: '${DatabaseHelper.columnId} = 1');
+
+    /*final deleteRecord = await dbHelper.delete(DatabaseHelper.table,
+        where: "${DatabaseHelper.columnId} = 1");*/
   }
 
   @override
